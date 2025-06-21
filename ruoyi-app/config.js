46a686c354c6509d1a1c322279d919b6abddc907
cdd <chendongdong@chinahikari.com>
2025-06-21 10:25:25 +0800

    添加小程序项目

---
+++ ruoyi-app/config.js
@@ -0,0 +1,26 @@
+// 应用全局配置
+module.exports = {
+  baseUrl: 'https://api.cdd9527.cn',
+  // baseUrl: 'http://localhost:8080',
+  // 应用信息
+  appInfo: {
+    // 应用名称
+    name: "ruoyi-app",
+    // 应用版本
+    version: "1.2.0",
+    // 应用logo
+    logo: "/static/logo.png",
+    // 官方网站
+    site_url: "https://ht.cdd9527.cn",
+    // 政策协议
+    agreements: [{
+        title: "隐私政策",
+        url: "/static/protocol.html"
+      },
+      {
+        title: "用户服务协议",
+        url: "/static/protocol.html"
+      }
+    ]
+  }
+}
